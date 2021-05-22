47285e2d8714ac271bb62c546ae3e58267b39801
Pomidorkin <pbriuzgin@gmail.com>
2021-05-22 11:18:28 +0900

    Placing defenders UI implementation

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class DefenderSpawner : MonoBehaviour
 {
-
-    [SerializeField] GameObject defender;
+    Defender defender;
 
     private void OnMouseDown()
     {
         SpawnDefender(GetSquareClicked());
+    }
+
+    public void SetSelectedDefender(Defender defenderToSelect)
+    {
+        defender = defenderToSelect;
     }
 
     private Vector2 GetSquareClicked()
@@ -29,6 +33,6 @@ public class DefenderSpawner : MonoBehaviour
 
     private void SpawnDefender(Vector2 spawnCoords)
     {
-        GameObject newDefender = Instantiate(defender, spawnCoords, Quaternion.identity) as GameObject;
+        Defender newDefender = Instantiate(defender, spawnCoords, Quaternion.identity) as Defender;
     }
 }
