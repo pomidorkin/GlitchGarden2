3446dfb8d05e4089750dc4e29763b548dcebf2b0
Pomidorkin <pbriuzgin@gmail.com>
2021-06-05 11:09:59 +0900

    Slider & level timer added

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -31,9 +31,8 @@ public class LevelLoader : MonoBehaviour
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadYouLose()
     {
-        
+        SceneManager.LoadScene("Lose Screen");
     }
 }
---
+++ Assets/Scripts/Lives.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lives : MonoBehaviour
+{
+
+    [SerializeField] int lives = 5;
+    Text livesText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        livesText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        livesText.text = lives.ToString();
+    }
+
+    public void TakeLife()
+    {
+            lives -= 1;
+            UpdateDisplay();
+
+        if(lives <= 0)
+        {
+            FindObjectOfType<LevelLoader>().LoadYouLose();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
